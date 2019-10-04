5cbbea79abf32440412bdb9dbdac9d320a6bc8f7
atheobold <atheobold@aol.com>
2019-10-04 13:42:37 -0600

    deleted matrix exercises and added neighbor collaboration to prompts

--- Student Version/PracticeSolutions.R
+++ Student Version/PracticeSolutions.R
@@ -1,26 +1,19 @@
 ###################################### Application Questions ##########################################  
 
-# Practice: Year was saved as an integer data type (1989 - 2006), but it is a factor. 
-# Write the `R` code to convert year to a factor (as you did with section and species), 
-#and verify that year is now viewed as a categorical variable, with the same levels as before. 
-#(hint: you have already used three functions that would do this for you)
-
-BlackfootFish$year <- as.factor(BlackfootFish$year)
-
-# There are many ways to verify the class of a variable:
-levels(BlackfootFish$year)
-str(BlackfootFish$year)
-summary(BlackfootFish$year)
-class(BlackfootFish$year)
-
-
-
 # Practice: Using statistics or graphics, which year in our dataset had the most fish caught?  
 
-# Statistics:  
-summary(BlackfootFish$year)
+##################### Statistics ##########################
+summary(BlackfootFish$yearF)
+# This assumes that you have a yearF variable in your data set, where you 
+# converted from numeric to a factor
 
-#Graphics:
+# OR 
+table(BlackfootFish$year)
+# This creates a table of the number of observations (rows) for each year in the dataset
+
+
+##################### Graphics ##########################
+# To create a bar plot of the number of observations each year
 year <- table(BlackfootFish$year)
 
 barplot(year, xlab = "Year", ylab = "Number of Fish", main = "Fish Caught by Year",
@@ -30,5 +23,9 @@ barplot(year, xlab = "Year", ylab = "Number of Fish", main = "Fish Caught by Yea
   
 # Practice: Make a boxplot of the fish weights over the different years in the dataset.  
 
-boxplot(weight ~ year, data = BlackfootFish2, xlab = "Year", ylab = "Weight (in grams)", 
-        main = "Fish Weight by Year", las = 1)
+boxplot(weight ~ year, 
+        data = BlackfootFish2, 
+        xlab = "Year", 
+        ylab = "Weight (in grams)", 
+        main = "Fish Weight by Year", 
+        las = 1)
